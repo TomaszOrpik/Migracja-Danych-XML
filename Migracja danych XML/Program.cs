845f0e4d95917099cace516727b2c591bb040246
TomaszOrpik <orpik.tomasz@gmail.com>
2020-02-27 23:12:38 +0100

    upload

---
+++ Migracja danych XML/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Migracja_danych_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Tomek\\Desktop\\cs201-lab-Migracja-danych-via-XML-LinqToXML-master\\issues.xml";
+            EksportImport ei = new EksportImport(path);
+            Console.WriteLine("Save complete");
+            Console.ReadKey();
+
+            /*
+             * otrzymuje path
+             * przetwarza
+             * daje console complete
+             * szukamy pliku i sprawdzamy
+             */
+        }
+    }
+}
